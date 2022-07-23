f8aaabac88322d779e30781325ffe622927da399
Pablo Solano <pablosolano61098@gmail.com>
2022-07-23 17:38:59 -0500

    Add base app layout

---
+++ src/App.styles.tsx
@@ -0,0 +1,29 @@
+// import styled from 'styled-components';
+
+// export const AppContainer = styled.div`
+
+import styled from 'styled-components';
+
+// `;
+
+export const AppContainer = styled.div`
+
+  width: 100vw;
+  display: flex;
+
+  justify-content: center;
+
+`;
+
+export const AppContent = styled.div`
+
+  width: 90vw;
+  background-color: red;
+
+  .centered{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.styles.tsx';
+import { AppContainer, AppContent } from './App.styles';
+import {Routes, Route} from 'react-router-dom';
+import HomePage from './pages/HomePage/Home.page';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <AppContent>
+        <Routes>
+          <Route path="/" element={<HomePage/>}>
+          </Route>
+        </Routes>
+      </AppContent>
+    </AppContainer>
   );
 }
 
