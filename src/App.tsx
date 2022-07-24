5856b83b0acaec8891fea1afed4446404f23089d
Pablo Solano <pablosolano61098@gmail.com>
2022-07-23 19:43:46 -0500

    Add routing to app
    Two routes right now. Admin and homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.styles.tsx';
-import { AppContainer, AppContent } from './App.styles';
-import {Routes, Route} from 'react-router-dom';
-import HomePage from './pages/HomePage/Home.page';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.styles.tsx";
+import { AppContainer, AppContent } from "./App.styles";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/Home.page";
+import { appRoutes } from "constants/routes/routes";
+import AdminPage from "pages/AdminPage/AdminPage.component";
 
 function App() {
   return (
     <AppContainer>
       <AppContent>
         <Routes>
-          <Route path="/" element={<HomePage/>}>
-          </Route>
+          <Route path={appRoutes.homePage} element={<HomePage />}></Route>
+          {/* <Route path={appRoutes.adminLogin} element={<LoginPage />}></Route>
+          <Route path={appRoutes.employeeLogin} element={<LoginPage />}></Route> */}
+          <Route path={appRoutes.admin} element={<AdminPage />}></Route>
         </Routes>
       </AppContent>
     </AppContainer>
