bc176719e4e20f9a82c77344b2e666377858dee8
Pablo Solano <pablosolano61098@gmail.com>
2022-07-24 15:15:02 -0500

    Define Employee object type

---
+++ src/types/dtos/employeeDto.ts
@@ -0,0 +1,16 @@
+export type EmployeeDto = {
+  id?: number;
+  names?: string;
+  lastnames?: string;
+  email?: string;
+  identification?: string;
+  birthdate?: string;
+  address?: string;
+  phone?: string;
+  vaccinationstate?: number;
+  vaccine?: number;
+  vaccinationdate?: string;
+  dosisnumber?: number;
+  username?: string;
+  password?: string;
+};
