a3068555faaf1cc64b145110d8b0c1d1d11c5eb9
Pablo Solano <pablosolano61098@gmail.com>
2022-07-24 16:14:50 -0500

    Add support to be sure not to add an employee with the same identification

--- src/api/employee/employeeApi.ts
+++ src/api/employee/employeeApi.ts
@@ -14,22 +14,38 @@ export const EmployeeApi = {
   },
 
   getEmployeeById: async ({ id_employee }: EmployeeApiGetById) => {
-    const employee = await getQuery(`${endPointRoute}${id_employee}`);
+    const employee = await getQuery(
+      `${endPointRoute}${employeeSlug}${id_employee}`
+    );
     return [employee] as EmployeeDto[];
   },
 
-  // createEmployee: async (new_employee: EmployeeDto) => {
-  //   const employee = await postQuery(`${endPointRoute}`, new_employee);
-  //   return [employee] as EmployeeDto[];
-  // },
+  createEmployee: async (new_employee: EmployeeDto) => {
+    const existingEmployee = await getQuery(
+      addFilterToQuery(`${endPointRoute}${employeeSlug}`, {
+        identification: new_employee.identification,
+      })
+    );
+    console.log(existingEmployee);
+    const isThereAEmployeeWithThatIdentification = existingEmployee.length > 0;
 
-  // updateEmployee: async (employee_beign_edited: EmployeeDto) => {
-  //   const employee = await putQuery(
-  //     `${endPointRoute}${employee_beign_edited.id}`,
-  //     employee_beign_edited
-  //   );
-  //   return [employee] as EmployeeDto[];
-  // },
+    if (isThereAEmployeeWithThatIdentification) {
+      return false;
+    }
+
+    const employee = isThereAEmployeeWithThatIdentification
+      ? ({} as EmployeeDto)
+      : await postQuery(`${endPointRoute}${employeeSlug}`, new_employee);
+    return [employee] as EmployeeDto[];
+  },
+
+  updateEmployee: async (employee_beign_edited: EmployeeDto) => {
+    const employee = await putQuery(
+      `${endPointRoute}${employeeSlug}${employee_beign_edited.id}`,
+      employee_beign_edited
+    );
+    return [employee] as EmployeeDto[];
+  },
 
   deleteEmployee: async (employee_beign_deleted: EmployeeDto) => {
     const employee = await deleteQuery(
@@ -37,4 +53,24 @@ export const EmployeeApi = {
     );
     return [employee] as EmployeeDto[];
   },
+};
+
+type filtersType = {
+  identification?: string;
+  vaccinationstate?: string;
+};
+
+const addFilterToQuery = (baseUrl: string, filters: filtersType) => {
+  let allUrl = baseUrl;
+  const filtersKeys = Object.keys(filters);
+  for (let i = 0; i < filtersKeys.length; i++) {
+    allUrl =
+      allUrl +
+      "?" +
+      filtersKeys[i] +
+      "=" +
+      filters[filtersKeys[i] as keyof filtersType];
+  }
+  console.log(allUrl);
+  return allUrl;
 };
