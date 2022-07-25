07d6f6ccd3687daf7fcd77a741ad51b10ec6be56
Pablo Solano <pablosolano61098@gmail.com>
2022-07-25 00:42:49 -0500

    Apply filter when loading data and according to vaccination status filter

--- src/api/employee/employeeApi.ts
+++ src/api/employee/employeeApi.ts
@@ -1,15 +1,40 @@
 import { endPointRoute, employeeSlug } from "constants/apiEndPoints";
+import { VaccinationStatusFilterEnum } from "enums/vaccinationStatus";
+import { VaccinationTypesEnum } from "enums/vaccinationTypes";
 import { EmployeeDto } from "types/dtos/employeeDto";
+import { FiltersType, VaccinationtypeType } from "types/filters";
 import { deleteQuery, getQuery, postQuery, putQuery } from "../querier";
+import {
+  addIdentificationFilterToQuery,
+  addVaccinationStateFilter,
+  addVaccinationTypeFilter,
+  searchForExistingEmployee,
+} from "./employeeApi.utils";
 
-type EmployeeApiGetAll = {};
+type EmployeeApiGetAll = {
+  filters: FiltersType;
+};
 type EmployeeApiGetById = {
   id_employee: string;
 };
 
 export const EmployeeApi = {
-  getEmployees: async ({}: EmployeeApiGetAll) => {
-    const allEmployee = await getQuery(`${endPointRoute}${employeeSlug}`);
+  getEmployees: async ({ filters }: EmployeeApiGetAll) => {
+    let query = `${endPointRoute}${employeeSlug}`;
+
+    let searchingForNotVaccinated =
+      filters.vaccinationstate.toString() !== VaccinationStatusFilterEnum.ALL;
+
+    query = addVaccinationStateFilter(
+      query,
+      filters.vaccinationstate.toString()
+    );
+    if (!searchingForNotVaccinated) {
+      query = addVaccinationTypeFilter(query, filters.vaccinationtype);
+    }
+
+    console.log(query);
+    const allEmployee = await getQuery(query);
     return allEmployee as EmployeeDto[];
   },
 
