6b1448e69a5b6bc474bab3cd670a2c3f76df11f9
Pablo Solano <pablosolano61098@gmail.com>
2022-07-25 00:34:32 -0500

    Add enums for vaccination types and vaccination status

---
+++ src/enums/vaccinationStatus.ts
@@ -0,0 +1,5 @@
+export enum VaccinationStatusFilterEnum {
+  NOT_VACCINATED = "0",
+  VACCINATED = "1",
+  ALL = "2",
+}
---
+++ src/enums/vaccinationTypes.ts
@@ -0,0 +1,7 @@
+export enum VaccinationTypesEnum {
+  NONE = "0",
+  SPUTNIK = "1",
+  ASTRAZENECA = "2",
+  PFIZER = "3",
+  JJ = "4",
+}
