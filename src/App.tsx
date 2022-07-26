05ab214a8a6ef9befa4ef63afa3976b0457c2a36
Pablo Solano <pablosolano61098@gmail.com>
2022-07-25 22:15:41 -0500

    Load login or editEmployee based on user status

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.styles.tsx";
 import { AppContainer, AppContent } from "./App.styles";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage/Home.page";
 import { appRoutes } from "constants/routes/routes";
 import AdminPage from "pages/AdminPage/AdminPage.component";
+import LoginPage from "pages/LoginPage/LoginPage.page";
+import { useUser } from "context/user/useUser";
+import EditEmployeePage from "pages/EditEmployeePage/EditEmployeePage.component";
+import MyButton from "components/MyButton/MyButton.component";
 
 function App() {
+  const { user, isThereUserLoggedIn: loadLogin } = useUser();
+  console.log(user);
   return (
     <AppContainer>
       <AppContent>
@@ -16,6 +22,10 @@ function App() {
           {/* <Route path={appRoutes.adminLogin} element={<LoginPage />}></Route>
           <Route path={appRoutes.employeeLogin} element={<LoginPage />}></Route> */}
           <Route path={appRoutes.admin} element={<AdminPage />}></Route>
+          <Route
+            path={appRoutes.employeeLogin}
+            element={loadLogin ? <EditEmployeePage /> : <LoginPage />}
+          ></Route>
         </Routes>
       </AppContent>
     </AppContainer>
